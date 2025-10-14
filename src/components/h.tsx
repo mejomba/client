e9256d0f0afc69327e421a70ecb60afaa93e9724
mejomba <moojaminzadeh@gmail.com>
2025-10-14 21:58:18 +0330

    remore useless commponent

--- src/components/h.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Hoo(){
-  return (
-    <h1 className="text-3xl bg-red-400 font-bold underline">
-      Hello world! from component
-    </h1>
-  )
-}
